cb27e1f5a9eaa2bebe1c57d9750f2007e8af8a5f
Borovskiy <borovskiykonst@yandex.ru>
2020-12-21 18:09:39 +0300

    add logger for Exception Handler

--- src/main/java/ru/bellintegrator/practice/controller/ResponseExceptionHandler.java
+++ src/main/java/ru/bellintegrator/practice/controller/ResponseExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.bellintegrator.practice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,14 +11,21 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import ru.bellintegrator.practice.dto.ApiResponse;
 import ru.bellintegrator.practice.dto.ErrorResponse;
 
+import java.util.UUID;
+
 @ControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    private static Logger log = LoggerFactory.getLogger(ResponseExceptionHandler.class);
     @ExceptionHandler(value = Exception.class)
     private ResponseEntity<ApiResponse> handleError(Exception ex){
         if(ex instanceof ResponseStatusException){
             ResponseStatusException statusException = (ResponseStatusException) ex;
             return new ResponseEntity<>(new ErrorResponse(statusException.getReason()), statusException.getStatus());
         }
-        return new ResponseEntity<>(new ErrorResponse("что-то пошло не так, обратитесь в техподдержку"), HttpStatus.INTERNAL_SERVER_ERROR);
+        else {
+            String uuid = UUID.randomUUID().toString();
+            log.error(uuid, ex);
+            return new ResponseEntity<>(new ErrorResponse("что-то пошло не так, обратитесь в техподдержку. ID ошибки: " + uuid), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
